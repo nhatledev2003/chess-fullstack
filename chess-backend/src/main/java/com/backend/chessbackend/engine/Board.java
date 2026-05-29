994594694b4316f60190eb8a3633f64ce4124596
nhatledev2003 <nhatledev2003@gmail.com>
2026-05-29 14:36:29 +0700

    Handle chess checking algorithm

--- chess-backend/src/main/java/com/backend/chessbackend/engine/Board.java
+++ chess-backend/src/main/java/com/backend/chessbackend/engine/Board.java
@@ -28,7 +28,41 @@ public class Board {
             }
         }
 
-        // 2. Setup quân cờ (Chúng ta sẽ thêm logic xếp quân vào đây ở Day 4 & Day 5
-        // sau khi đã tạo xong các class con như Pawn, Knight, King...)
+
+
+
+
+    }
+    // 2. Setup quân cờ (Chúng ta sẽ thêm logic xếp quân vào đây ở Day 4 & Day 5
+    // sau khi đã tạo xong các class con như Pawn, Knight, King...)
+    public boolean isInCheck(Color color) {
+        Square kingSquare = findKingSquare(color);
+
+        // Trải phẳng vòng lặp để quét 64 ô cờ
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Piece currentPiece = squares[i][j].getPiece();
+
+                // Nếu ô này có chứa quân cờ của đối phương
+                if (currentPiece != null && currentPiece.getColor() != color) {
+                    // Mượn logic isValidMove của chính quân địch để xem nó có "ăn" được Vua không
+                    if (currentPiece.isValidMove(this, squares[i][j], kingSquare)) {
+                        return true; // Phát hiện tia chiếu!
+                    }
+                }
+            }
+        }
+        return false; // An toàn
+    }
+    private Square findKingSquare(Color color) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Piece piece = squares[i][j].getPiece();
+                if (piece != null && piece instanceof King && piece.getColor() == color) {
+                    return squares[i][j];
+                }
+            }
+        }
+        throw new IllegalStateException("Lỗi nghiêm trọng: Không tìm thấy Vua trên bàn cờ!");
     }
 }
